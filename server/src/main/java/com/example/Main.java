8d9dd373636a24626b4c04752289e2da986fa865
lai2-24 <lai2-24@itismeucci.com>
2023-12-11 11:00:49 +0100

    commit 0

---
+++ server/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+import java.net.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Response response = new Response(8080);
+        while (true) {
+            response.recive();
+        }
+        
+
+    }
+}
---
+++ server/src/main/java/com/example/Response.java
@@ -0,0 +1,80 @@
+package com.example;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Response {
+    ServerSocket server;
+    Socket client = null;
+    DataOutputStream out;
+    BufferedReader in;
+    String[] parts;
+    Date date = new Date();
+    String readLine;
+    String firstLine;
+
+    public Response(int port) {
+        try {
+            ServerSocket server = new ServerSocket(port);
+            client = server.accept();
+            out = new DataOutputStream(client.getOutputStream());
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void recive(){
+        try {
+            do {
+                readLine = in.readLine();
+                if (readLine.contains("GET")) {
+                    firstLine = readLine;
+                }
+                System.out.println(readLine);
+            } while (!readLine.equals(""));
+            respond(firstLine);
+        } catch(NullPointerException e){
+            
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        } 
+    }   
+    //htttp response
+    public void respond(String message){
+        if (message == null) {
+            return;
+        }
+        parts = message.split(" ");
+
+        try {
+            File file = new File("server/src/main/java/com/example" + parts[1]);
+            Scanner MyReader = new Scanner(file);
+            while (MyReader.hasNextLine()) {
+                out.writeBytes(MyReader.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            fileNotFound();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean findFile(String path){
+        File file = new File(path);
+        return file.isFile();
+    }
+    public void fileNotFound(){
+        try {
+                out.writeBytes("HTTP/1.1 404 Not Found\n");
+                out.writeBytes("Date: " + date.toString() + "\n");
+                out.writeBytes("content-length: 26\n");
+                out.writeBytes("Server: meucci-server\n");
+                out.writeBytes("Content-Type: text/plain; charset=UTF-8\n");
+                out.writeBytes("\n");
+                out.writeBytes("The resource was not found\n");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+    }
+}
